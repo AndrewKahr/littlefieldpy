d8b04009f7fb091cf8b85db11900e088bb5a2615
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2023-02-02 13:40:05 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup
 
+classifiers = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Education',
+  'Topic :: Education',
+  'Operating System :: Microsoft :: Windows',
+  'Operating System :: MacOS',
+  'Operating System :: POSIX :: Linux',
+  'License :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
 setup(name='littlefieldpy',
       version='0.0.1',
       description='API for the Littlefield simulation',
+      long_description=open('README.md').read() + '\n\n',
       url='http://github.com/AndrewKahr/littlefieldpy',
+      keywords='littlefield',
+      classifiers=classifiers,
       author='Andrew Kahr',
       license='MIT',
       packages=['littlefieldpy'],
